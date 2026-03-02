a36a90b56ec9cb616e37470b4a6f22592b42a47f
Ashish Sakhuja <axs7262@psu.edu>
2026-03-02 11:12:56 -0500

    Updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes.news import router as news_router
 from .routes.meta import router as meta_router
 from .routes.feed import router as feed_router
@@ -8,6 +9,19 @@ from app.routes.nfl import router as nfl_router
 from app.routes.analytics import router as analytics_router
 
 app = FastAPI(title="SportsHub API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://10.0.0.219:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(news_router)
 app.include_router(meta_router)
 app.include_router(feed_router)
